3ad1a09d14dcf596cc4e45ff875a68467f49d41d
ShutaHayamizu <hayamizu@japan-architect.co.jp>
2026-03-10 18:17:51 +0900

    データ処理用のコードを作成（開始）

--- data_editing/data_collecting.py
+++ data_editing/data_collecting.py
@@ -10,16 +10,58 @@ client_gs = config[0]
 client_bq = config[1]
 client_dv = config[2]
 
+# ライブラリ
+import os
+import re
+import os.path
+import json
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from pprint import pprint
 
-base_sheet_key = '1-tMiiPQiB5zXqdP5PcfHWOpSnoc1fmydVlwFONJjogk'
-address_sheet_key = '1rXjOAkpL6GfyO6mWxwwUR0PyFtuNHrt7X3QoHY1uN3k'
+import gspread
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
 
 
-
+# config
+keys = [{
+            "sheetname":"UK List",
+            "key":"1-tMiiPQiB5zXqdP5PcfHWOpSnoc1fmydVlwFONJjogk"
+         },
+         {
+            "sheetname":"SELECT_all",
+            "key":"1rXjOAkpL6GfyO6mWxwwUR0PyFtuNHrt7X3QoHY1uN3k"
+         }]
 
 def main():
 
 
+    pass
+
+
+
+
+
+def getSheetDF(keys):
+    data = []
+    for key in keys:
+        book = client_gs.open_by_key(key["key"])
+        sheet = book.worksheet(key["sheetname"])
+        data = sheet.get_all_values()
+        df = pd.DataFrame(data[1:], columns=data[0])
+        d = {"name":key["sheetname"], "data":df}
+        data.append(d)
+    
+    df_base = None
+    df_add = None
+    for d in data:
+        if d["name"] == 'UK List':
+            df_base = d["data"]
+        else:
+            df_add = d["data"]
+    
+    return df_base, df_add
 
 
 if __name__ == "__main__":
